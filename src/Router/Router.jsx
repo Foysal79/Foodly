e778ec8bde65b3a32b970d09a41de946f048e687
Foysal79 <onthor312001@gmail.com>
2023-12-17 14:07:09 +0600

    First Commit and project set up

---
+++ src/Router/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../LayOut/Root/Root";
+import Home from "../LayOut/Home/Home";
+
+const Router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root></Root>,
+      children : [
+        {
+            path : "/",
+            element : <Home></Home>
+        }
+      ]
+    },
+  ]);
+
+export default Router;
