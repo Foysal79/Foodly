ae179cb1fd093c13b8c9aeb324fa719a602b9960
Foysal79 <onthor312001@gmail.com>
2024-01-05 01:55:37 +0600

    footer session modified

--- src/Components/TrendingProducts/TrendingProducts.jsx
+++ src/Components/TrendingProducts/TrendingProducts.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+
 import DynamicTitle from '../../Share/DainamicTitle/DynamicTitle';
 
+
 const TrendingProducts = () => {
+    
     return (
         <div>
             <DynamicTitle heading={"Trending Products"} subHeading={"Recently added our store"} ></DynamicTitle>
+
+
             
         </div>
     );
--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -2,33 +2,62 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <footer className="footer p-10 bg-base-200 text-base-content">
+        <div className='font-Mali' >
+  <div className='bg-base-200 flex lg:px-28 py-5 '>
+    <div className='w-4/12 mx-auto ' >
+     <img className='w-36 lg:ml-5 mb-5 ' src="https://i.ibb.co/K97VxGn/Green-Leaf-Aesthetic-Organic-Skincare-Logo-removebg-preview.png" alt="" />
+     <p className='text-left' >The variety of products available at our store at the moment is vast, but we still continue to widen our assortment.</p>
+
+    </div>
+
+
+  <footer className="footer p-10 bg-base-200 text-base-content">
   <nav>
-    <header className="footer-title">Services</header> 
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
+    <header className=" text-black font-bold text-lg mb-4 ">Information</header> 
+    <a className="link link-hover">About Us</a>
+    <a className="link link-hover">Contact Us</a>
+    <a className="link link-hover">Wishlist</a>
+    <a className="link link-hover">Portfolio</a>
+    <a className="link link-hover">Privacy Policy</a>
+    <a className="link link-hover">Frequently</a>
   </nav> 
   <nav>
-    <header className="footer-title">Company</header> 
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
+    <header className="text-black font-bold text-lg mb-4 ">Categories</header> 
+    <a className="link link-hover">All Products</a>
+    <a className="link link-hover">Grocery & Frozen</a>
+    <a className="link link-hover">Fresh Fruits</a>
+    <a className="link link-hover">Package Foods</a>
+    <a className="link link-hover">Organic Foods</a>
+    <a className="link link-hover">Health & Wellness</a>
   </nav> 
   <nav>
-    <header className="footer-title">Legal</header> 
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
+    <header className="text-black font-bold text-lg mb-4 ">My Account</header> 
+    <a className="link link-hover">My Account</a>
+    <a className="link link-hover">Shopping Cart</a>
+    <a className="link link-hover">Checkout</a>
+    <a className="link link-hover">Login</a>
+    <a className="link link-hover">Register</a>
+    <a className="link link-hover">Compare</a>
   </nav>
-</footer> 
+  <form>
+    <header className="text-black font-bold text-lg mb-4 ">Newsletter</header> 
+    <fieldset className="form-control w-80">
+      <label className="label">
+        <span className="label-text">Enter your email address</span>
+      </label> 
+      <div className="join">
+        <input type="text" placeholder="username@site.com" className="input input-bordered join-item" /> 
+        <button className="btn btn-primary join-item">Subscribe</button>
+      </div>
+    </fieldset>
+  </form>
+</footer>
+
+  </div> 
 <footer className="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
   <aside className="items-center grid-flow-col">
-    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-    <p>ACME Industries Ltd. <br/>Providing reliable tech since 1992</p>
+    <img className='w-14' src="https://i.ibb.co/K97VxGn/Green-Leaf-Aesthetic-Organic-Skincare-Logo-removebg-preview.png" alt="" />
+    <p>Foodly Industries Ltd. <br/>Providing reliable tech since 1992</p>
   </aside> 
   <nav className="md:place-self-center md:justify-self-end">
     <div className="grid grid-flow-col gap-4">
